c264cc1671c50d06a2d2799fbcfd406d657e754c
moze30 <z834080913@163.com>
2026-04-18 08:39:50 +0000

    修复编译错误

--- app/src/main/java/com/winlator/inputcontrols/ControlElement.java
+++ app/src/main/java/com/winlator/inputcontrols/ControlElement.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.RectF; 
 
 import androidx.core.graphics.ColorUtils;
 
