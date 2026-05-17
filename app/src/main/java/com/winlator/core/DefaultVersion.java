cded725eb7899bae9a0e8f33d559300b745cb9f1
moze30 <z834080913@163.com>
2026-05-17 08:16:12 +0000

    适配

--- app/src/main/java/com/winlator/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/core/DefaultVersion.java
@@ -2,7 +2,7 @@ package com.winlator.glibc.core;
 
 public abstract class DefaultVersion {
     public static final String BOX64 = "0.4.1";
-    public static final String TURNIP = "26.1.0";
+    public static final String TURNIP = "26.2.0";
     public static final String ZINK = "22.2.5";
     public static final String VIRGL = "23.1.9";
     public static final String DXVK = "2.3.1";
